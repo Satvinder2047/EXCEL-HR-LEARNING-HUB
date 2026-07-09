901bdfdbe9512f24ef25f7bc915712f208e2009b
Satvinder2047 <g.singh18695@gmail.com>
2026-07-09 18:56:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -132,3 +132,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+// ===========================
+// Reading Progress Bar
+// ===========================
+
+window.addEventListener("scroll", function () {
+
+let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+let scrolled = (winScroll / height) * 100;
+
+let progress = document.getElementById("progressBar");
+
+if(progress){
+
+progress.style.width = scrolled + "%";
+
+}
+
+});
